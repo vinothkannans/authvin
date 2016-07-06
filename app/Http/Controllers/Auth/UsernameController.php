6234a3084a0b92951d0be39888ad56d15ca91cd4
Vinoth Kannan <vinothkannan@vinkas.com>
2016-07-06 19:53:52 +0530

    Username controller added

---
+++ app/Http/Controllers/Auth/UsernameController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Authvin\Http\Controllers\UsernameController as AuthvinController;
+
+class UsernameController extends AuthvinController
+{
+
+}
---
+++ authvin/Http/Controllers/UsernameController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Authvin\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+abstract class UsernameController extends Controller
+{
+}
